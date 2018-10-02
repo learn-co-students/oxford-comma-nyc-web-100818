458937fa2091243b72c92577d7e6af45804cc792
tirempbs <tirempbs@gmail.com>
2018-10-01 22:11:26 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length >= 3
+    last_part = "and #{array[-1]}"
+    array.pop
+    array.push(last_part)
+    return array.join(", ")
+  end
 end
